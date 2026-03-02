212e453c0cf6c780e532da10f1b326cc8e5cb3e0
leonardo <leonardodamascenodev@gmail.com>
2026-03-02 17:42:54 -0400

    added new migrations in lib.rs

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -4,6 +4,8 @@ mod m20260221_225946_tb_player;
 mod m20260221_230000_tb_line;
 mod m20260221_230100_tb_line_player;
 mod m20260221_230200_tb_confrontation;
+mod m20260302_212912_tb_user;
+mod m20260302_213032_tb_group;
 
 pub struct Migrator;
 
@@ -15,6 +17,8 @@ impl MigratorTrait for Migrator {
             Box::new(m20260221_230000_tb_line::Migration),
             Box::new(m20260221_230100_tb_line_player::Migration),
             Box::new(m20260221_230200_tb_confrontation::Migration),
+            Box::new(m20260302_212912_tb_user::Migration),
+            Box::new(m20260302_213032_tb_group::Migration),
         ]
     }
 }
