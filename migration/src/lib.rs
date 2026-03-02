4b31e8f26708e2a0c545210f8f18e99ceec39cbe
leonardo <leonardodamascenodev@gmail.com>
2026-03-02 19:45:43 -0400

    added a new migration tb_user

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -4,7 +4,6 @@ mod m20260221_225946_tb_player;
 mod m20260221_230000_tb_line;
 mod m20260221_230100_tb_line_player;
 mod m20260221_230200_tb_confrontation;
-mod m20260302_212912_tb_user;
 mod m20260302_213032_tb_group;
 mod m20260302_215142_add_group_tb_line;
 mod m20260302_220419_add_group_tb_player;
@@ -21,7 +20,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20260221_230000_tb_line::Migration),
             Box::new(m20260221_230100_tb_line_player::Migration),
             Box::new(m20260221_230200_tb_confrontation::Migration),
-            Box::new(m20260302_212912_tb_user::Migration),
+            Box::new(m20260302_232438_tb_user::Migration),
             Box::new(m20260302_213032_tb_group::Migration),
             Box::new(m20260302_215142_add_group_tb_line::Migration),
             Box::new(m20260302_220419_add_group_tb_player::Migration),
