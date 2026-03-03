6b9aa150d0db75635c8e087931ccb1e10f77a2e6
leonardo <leonardodamascenodev@gmail.com>
2026-03-03 04:10:24 -0400

    added column "email" in table tb_user

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -9,6 +9,7 @@ mod m20260302_215142_add_group_tb_line;
 mod m20260302_220419_add_group_tb_player;
 mod m20260302_223728_add_group_tb_confrontation;
 mod m20260302_232438_tb_user;
+mod m20260303_080705_add_column_email_tb_user;
 
 pub struct Migrator;
 
@@ -20,12 +21,12 @@ impl MigratorTrait for Migrator {
             Box::new(m20260221_230000_tb_line::Migration),
             Box::new(m20260221_230100_tb_line_player::Migration),
             Box::new(m20260221_230200_tb_confrontation::Migration),
-            Box::new(m20260302_232438_tb_user::Migration),
             Box::new(m20260302_213032_tb_group::Migration),
             Box::new(m20260302_215142_add_group_tb_line::Migration),
             Box::new(m20260302_220419_add_group_tb_player::Migration),
             Box::new(m20260302_223728_add_group_tb_confrontation::Migration),
             Box::new(m20260302_232438_tb_user::Migration),
+            Box::new(m20260303_080705_add_column_email_tb_user::Migration),
         ]
     }
 }
---
+++ migration/src/m20260303_080705_add_column_email_tb_user.rs
@@ -0,0 +1,31 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table("tb_user")
+                    .add_column(string_len_uniq("email", 100))
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table("tb_user")
+                    .drop_column("email")
+                    .to_owned(),
+            )
+            .await
+    }
+}
