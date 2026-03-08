13e00a9200f3ff42f31eb4850d4693e2d1938568
leonardo <leonardodamascenodev@gmail.com>
2026-03-08 10:33:37 -0400

    added new dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod services;
 
 use actix_cors::Cors;
 use actix_web::{App, HttpServer, http::header, web};
+use dotenv::dotenv;
 use sea_orm::Database;
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -12,6 +13,8 @@ use router::{confrontation_router, line_router, player_router};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     tracing_subscriber::registry()
         .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()))
         .with(tracing_subscriber::fmt::layer())
